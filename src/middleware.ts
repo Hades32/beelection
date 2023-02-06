4b01adfc54c6d930113617434d05dc592184bd56
Martin Rauscher <martin@ultimatetournament.io>
2023-02-07 00:06:23 +0100

    scale to zero

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+// middleware.ts
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const killFunc = () => {
+  console.log("nobody is here. Shutting down...");
+  process.exit(0);
+};
+let killSwitch = setInterval(killFunc, 30_000);
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request: NextRequest) {
+  clearInterval(killSwitch);
+  setInterval(killFunc, 30_000);
+}
